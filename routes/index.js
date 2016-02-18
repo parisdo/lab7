0999c3efc1b5e4f697a799b12b74de8ac01753cc
Paris Do <pcdo@ucsd.edu>
2016-02-18 04:03:54 +0000

    analytics lab

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,17 @@ var projects = require('../projects.json');
  * GET home page.
  */
 
-exports.view = function(req, res){
-  	res.render('index', projects);
-};
+ exports.view = function(req, res){
+ 	var random_num = Math.random();
+   	if (random_num > 0.5) {
+   		projects["grid"] = false;
+   		res.render('index', projects);
+   	} else {
+   		res.redirect('/grid');
+   	}
+ };
+
+ exports.viewGrid = function(req, res){
+ 	projects["grid"] = true;
+ 	res.render('index', projects);
+ };

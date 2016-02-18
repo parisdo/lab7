ba7428d28c9fc8d610e3682862288141b36d508b
Paris Do <pcdo@ucsd.edu>
2016-02-18 04:17:07 +0000

    Added Experiment Header

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,15 @@ var projects = require('../projects.json');
  */
 
  exports.view = function(req, res){
- 	var random_num = Math.random();
-   	if (random_num > 0.5) {
-   		projects["grid"] = false;
-   		res.render('index', projects);
-   	} else {
-   		res.redirect('/grid');
-   	}
+   var random_num = Math.random();
+   console.log(random_num);
+
+   if (random_num >= 0) {
+     projects['grid'] = false;
+     res.render('index', projects);
+   } else {
+     res.redirect('/grid');
+   }
  };
 
  exports.viewGrid = function(req, res){
